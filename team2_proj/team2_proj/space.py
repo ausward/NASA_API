ecbee64c0aa1bca9fde62cb02cabac8fa43799eb
Austin Ward <austinward@isu.edu>
2024-02-29 15:20:33 -0700

    I created a mod that will get the nasa POTD adn based on its title it gets a few wiki articales that provided contect to the photo. I added it as an endpoint "/POTD" and added the needed dependencys to poetry needed to run it.

--- team2_proj/team2_proj/space.py
+++ team2_proj/team2_proj/space.py
@@ -1,21 +1,29 @@
 from quart import Quart, jsonify, request
-# import request
+import requests
+import POTD
 
 app = Quart(__name__)
 
 @app.route('/nasa')
 async def get_nasa_data():
     nasa_api_url = 'https://api.nasa.gov/planetary/apod?api_key=YOUR_API_KEY'
-    response = request.get(nasa_api_url)
+    response = requests.get(nasa_api_url)
     data = response.json()
     return jsonify(data)
 
-@app.route('/wikipedia')
-async def get_wikipedia_data():
-    wikipedia_api_url = 'https://en.wikipedia.org/api/rest_v1/page/summary/Python_(programming_language)'
-    response = request.get(wikipedia_api_url)
+@app.route('/wikipedia/<keyword>')
+async def get_wikipedia_data(keyword):
+    wikipedia_api_url = f'https://en.wikipedia.org/api/rest_v1/page/summary/{keyword}'
+    response = requests.get(wikipedia_api_url)
     data = response.json()
+    if 'title' not in data:
+        return jsonify({'error': 'No Wikipedia page found'})
     return jsonify(data)
+
+@app.route('/POTD')
+async def get_potd():
+    return jsonify(POTD.get_POTD_with_desc())
+    
 
 
 if __name__ == '__main__':
@@ -23,7 +31,7 @@ if __name__ == '__main__':
     @app.route('/spacefact/<keyword>')
     async def get_space_fact(keyword):
         nasa_api_url = f'https://api.nasa.gov/planetary/apod?api_key=YOUR_API_KEY&search={keyword}'
-        response = request.get(nasa_api_url)
+        response = requests.get(nasa_api_url)
         data = response.json()
         if 'error' in data:
             return jsonify({'error': 'No space fact found'})
@@ -32,7 +40,7 @@ if __name__ == '__main__':
     @app.route('/wikipedia/<keyword>')
     async def get_wikipedia_data(keyword):
         wikipedia_api_url = f'https://en.wikipedia.org/api/rest_v1/page/summary/{keyword}'
-        response = request.get(wikipedia_api_url)
+        response = requests.get(wikipedia_api_url)
         data = response.json()
         if 'title' not in data:
             return jsonify({'error': 'No Wikipedia page found'})
